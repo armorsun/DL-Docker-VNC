44d8dbc61ce6591ac51efa0c55f9faa040852131
Sunny <gsunny4178@gmail.com>
2018-02-21 18:54:12 +0800

    update startup.sh and xstartup

--- startup.sh
+++ startup.sh
@@ -1,7 +1,15 @@
-#!/bin/env bash
+#!/bin/bash
+jupyter notebook & 
 
-#jupyter notebook "$@"
-
-if [-f /tmp/.X0-lock && -f /tmp/.X11-unix/X0]; then
-	rm /tmp/.X0-lock /tmp/.X11-unix/X0
+#setting up vncserver
+if [[ -e /tmp/.X0-lock && -e /tmp/.X11-unix/X0 ]]; then
+	rm -f /tmp/.X0-lock /tmp/.X11-unix/X0
 fi
+
+if [[ -e /home/ubuntu/.vnc/*.pid && -e /home/ubuntu/.vnc/*.log ]]; then
+	rm -f /home/ubuntu/.vnc/*.pid /home/ubuntu/.vnc/*.log
+fi
+
+xdg-mime default nemo.desktop inode/directory
+
+vncserver $1 $2 :0 && tail -f /home/ubuntu/.vnc/*.log 
